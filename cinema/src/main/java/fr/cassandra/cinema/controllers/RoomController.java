b1119abc3b906dfa492d34f49d9db27ae67f3a95
Cassandra <cassandra.commsi@gmail.com>
2021-12-17 10:08:10 +0100

    Salle - update

--- cinema/src/main/java/fr/cassandra/cinema/controllers/RoomController.java
+++ cinema/src/main/java/fr/cassandra/cinema/controllers/RoomController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,10 @@ public class RoomController {
 	@GetMapping
 	public List<Room> getRooms() {
 		return this.roomService.findAll();	
+	}
+	
+	@PutMapping
+	public Room updateRoom(@RequestBody Room room) {
+		return this.roomService.save(room);
 	}
 }
