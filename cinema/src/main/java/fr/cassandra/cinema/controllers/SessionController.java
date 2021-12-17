332c98deb95f0404d668947bd9ce6dbf4afb1751
Cassandra <cassandra.commsi@gmail.com>
2021-12-17 10:56:32 +0100

    Seance - update

--- cinema/src/main/java/fr/cassandra/cinema/controllers/SessionController.java
+++ cinema/src/main/java/fr/cassandra/cinema/controllers/SessionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,10 @@ public class SessionController {
 	@GetMapping
 	public List<Session> getSessions() {
 		return this.sessionService.findAll();
+	}
+	
+	@PutMapping
+	public Session updateSession(@RequestBody Session session) {
+		return this.sessionService.save(session);
 	}
 }
